57d2b23924f7db803533da8229171d6813abb01b
Adriano Ferrari <adriano.ferrari@gmail.com>
2016-09-12 12:07:21 -0400

    Compiles with new types. Displays structure to one level deep.

--- Main.elm
+++ Main.elm
@@ -7,7 +7,7 @@ import Html.App as App
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Json.Decode as Json
-import List.Extra exposing (find, interweave)
+import List.Extra exposing (find)
 import Task
 
 
@@ -27,66 +27,71 @@ main =
 
 
 type alias Model =
-  { structure : List Column 
-  , active : Coords
-  , editing : Maybe Coords
+  { structure : X
+  , active : (Int, Int, Int)
   }
   
 type alias Data =
-  { cards : List Card
-  , trees : List Tree
-  , rootTreeId : TreeId
-  , active : Coords
-  , editing : Maybe Coords
+  { content : List Content
+  , aList : List A
+  , active : (Int, Int, Int)
+  , root : Id
   }
 
-type alias Card =
-  { id : CardId
+type alias Content =
+  { id : Id
   , contentType : String
   , content : String
   }
 
-type alias Tree =
-  { id : TreeId
-  , rootCardId : CardId
+type alias A =
+  { id : Id
+  , content : Id
   , children : List Id
   }
 
+type X = 
+  X
+    { id : Id
+    , content : Content
+    , children : List X
+    }
 
-type alias Group = List Card
-
-type alias Column = List Group
-
-
--- Convenience types
 
 type alias Id = Int
-type alias CardId = Int
-type alias TreeId = Int
-type alias Coords = (Int, Int, Int)
 
-emptyModel : Model
-emptyModel =
-  { structure = [ [ [ emptyCard ] ] ]
+
+defaultModel : Model
+defaultModel =
+  { structure = defaultStructure
   , active = (0,0,0)
-  , editing = Nothing
   }
 
-emptyCard : Card
-emptyCard =
-  Card 0 "text/markdown" ""
+defaultContent : Content
+defaultContent =
+  { id = 0
+  , contentType = "text/markdown"
+  , content = "defaultContent"
+  }
 
-emptyTree : Tree
-emptyTree =
-  Tree 0 0 []
+
+defaultStructure : X
+defaultStructure =
+  X { id = 0
+    , content = defaultContent
+    , children= [ X {id = 1, content = defaultContent, children = []}
+                , X {id = 2, content = defaultContent, children = []}
+                ]
+    }
 
 init : Maybe Data -> ( Model, Cmd Msg )
 init savedData =
   case savedData of
     Nothing ->
-      emptyModel ! [ ]
+      defaultModel ! [ ]
     Just data ->
       ( buildModel data ) ! [ ]
+
 
 
 
@@ -111,44 +116,92 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div
-    [ ]
-    [ text "test" ]
+  viewX defaultModel.structure
 
 
-
-viewCard : Bool -> Bool -> Card -> Html Msg
-viewCard isActive isEditing card =
-  div
-    [ classList [ ( "card", True )
-                , ( "active", isActive )
-                , ( "editing", isEditing )
-                ]
-    , id ( "card-" ++ toString card.id )
-    ]
-    [ div [ class "view" ] [ text card.content ]
-    , input [ id ( "card-edit-" ++ toString card.id )
-            , class "edit"
-            , value card.content
+viewX : X -> Html Msg
+viewX x =
+  case x of
+    X x' ->
+      if x'.children == [] then
+        div [ id "app" ] -- root Only
+            [ div [ class "column" ]
+                  [ div [ class "group" ]
+                        [ viewXContent x ]
+                  ]
             ]
-            []
-    ]
+      else
+        div [ id "app" ]
+            [ div [ class "column" ]
+                  [ div [ class "group" ]
+                        [ viewXContent x ]
+                  ]
+            , div [ class "column" ]
+                  [ viewListX x'.children ]
+            ]
 
 
--- HELPERS
+viewXContent : X -> Html Msg
+viewXContent x =
+  case x of
+    X x' ->
+      div [ class "card" ]
+          [ text x'.content.content ]
+    
+
+viewListX : List X -> Html Msg
+viewListX xs =
+  div [ class "group" ]
+      (List.map viewXContent xs)
+
+
+-- STRUCTURING
+
+
+getId : X -> Id
+getId xin =
+  case xin of
+    X x ->
+      x.id
+
+
+xToA : X -> A
+xToA x =
+  case x of
+    X x' ->
+      { id = x'.id
+      , content = x'.content.id
+      , children = List.map getId x'.children
+      }
+
+
+aToX : Data -> A -> X
+aToX data a =
+  let
+    fmFunction id = find (\a -> a.id == id) data.aList -- (Id -> Maybe A)
+  in
+    X { id = a.id
+      , content = data.content  |> find (\c -> c.id == a.content)
+                                |> Maybe.withDefault defaultContent
+      , children = a.children -- List Id
+                    |> List.filterMap fmFunction -- List A
+                    |> List.map (aToX data) -- List X
+      }
 
 
 buildModel : Data -> Model
 buildModel data =
-  let
-    rootTree = data.trees |> find (\tree -> tree.id == data.rootTreeId)
-                          |> Maybe.withDefault emptyTree
-    rootCard = find (\card -> card.id == rootTree.rootCardId) data.cards |> Maybe.withDefault emptyCard
-  in
-    { structure = [[[rootCard]]]
-    , active = data.active
-    , editing = data.editing
-    }
+  { structure = data.aList -- List A
+                  |> find (\a -> a.id == data.root) -- Maybe A
+                  |> Maybe.withDefault (A 0 0 []) -- A
+                  |> aToX data -- X
+  , active = data.active
+  }
+
+
+
+
+--HELPERS
 
 
 onEnter : Msg -> Attribute Msg
