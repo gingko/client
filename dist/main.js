e596fd92d76ebf3e442e0a8396f87a4bf0630c74
Adriano Ferrari <adriano.ferrari@gmail.com>
2016-09-28 21:17:45 -0400

    Added react-commits-graph, and split out javascript from index.html

---
+++ dist/main.js
@@ -0,0 +1,402 @@
+var db = new PouchDB('elm-gingko-test-cards');
+var viewState = JSON.parse(localStorage.getItem('elm-gingko-viewState'));
+var commit = localStorage.getItem('elm-gingko-commit');
+var gingko = null;
+
+db.allDocs({include_docs: true}).then(function(docs){
+  contents = docs.rows
+            .filter(function(row){
+              return row.doc.type == "content" 
+            })
+            .map(function(row){
+              row.doc["id"] = row.doc["_id"];
+              delete row.doc["_id"];
+              delete row.doc["_rev"];
+              delete row.doc["type"];
+              return row.doc;
+            });  
+
+  nodes = docs.rows
+            .filter(function(row){
+              return row.doc.type == "node" 
+            })
+            .map(function(row){
+              row.doc["id"] = row.doc["_id"];
+              delete row.doc["_id"];
+              delete row.doc["_rev"];
+              delete row.doc["type"];
+              return row.doc;
+            });  
+
+  commits = docs.rows
+            .filter(function(row){
+              return row.doc.type == "commit" 
+            })
+            .map(function(row){
+              row.doc["id"] = row.doc["_id"];
+              delete row.doc["_id"];
+              delete row.doc["_rev"];
+              delete row.doc["type"];
+              return row.doc;
+            });  
+
+  operations = docs.rows
+            .filter(function(row){
+              return row.doc.type == "operation" 
+            })
+            .map(function(row){
+              row.doc["id"] = row.doc["_id"];
+              delete row.doc["_id"];
+              delete row.doc["_rev"];
+              delete row.doc["type"];
+              return row.doc;
+            });  
+            
+  
+  var startingState = null;
+
+  if (contents.length > 0 && nodes.length > 0 && commits.length > 0 ) {
+    startingState =
+      { objects :
+        { contents: contents
+        , nodes: nodes
+        , commits : commits
+        , operations : operations
+        }
+      , commit: commit ? commit : "bc5ff95381ff8577663e45455b14cd09d7e126c1"
+      , viewState: viewState ? viewState : {active: "0", editing: null, field: ""}
+      }
+  }
+
+  startElm(startingState);
+
+}).catch(function(err){
+  startElm(null);
+});
+
+startElm = function(init) {
+  gingko = Elm.Main.fullscreen(init);
+  gingko.ports.saveContents.subscribe(saveContents);
+  gingko.ports.saveNodes.subscribe(saveNodes);
+  gingko.ports.saveCommit.subscribe(saveCommit);
+  gingko.ports.saveCurrentCommit.subscribe(saveCurrentCommit);
+  gingko.ports.saveOp.subscribe(saveOp);
+  gingko.ports.activateCard.subscribe(activateCard);
+}
+
+scrollTo = function(cid) {
+  var card = document.getElementById('card-' + cid.toString());
+  var col = card.closest('.column');
+  if (card == null) return;
+  var rect = card.getBoundingClientRect();
+
+  TweenMax.to(col, 0.35,
+    { scrollTop: col.scrollTop + ((rect.top + rect.height*0.5) - col.offsetHeight*0.5)
+    , ease: Power2.easeInOut
+    });
+}
+
+
+saveContents = function(contents) {
+  data = contents.map(function(c){
+    return  { _id: c.id
+            , content: c.content
+            , contentType : c.contentType
+            , type : "content"
+            }
+  });
+  db.bulkDocs(data)
+    .catch(function(err){console.log(err)});
+}
+
+saveNodes = function(nodes) {
+  data = nodes.map(function(c){
+    return  { _id: c.id
+            , contentId: c.contentId
+            , childrenIds : c.childrenIds
+            , type : "node"
+            }
+  });
+  db.bulkDocs(data)
+    .catch(function(err){console.log(err)});
+}
+
+saveCommit = function(commit) {
+  commit["_id"] = commit["id"];
+  delete commit["id"];
+  commit["type"] = "commit";
+  db.put(commit)
+    .catch(function(err){console.log(err)});
+}
+
+saveCurrentCommit = function(currentCommit) {
+  console.log(currentCommit);
+  localStorage.setItem("elm-gingko-commit", currentCommit);
+}
+
+saveOp = function(op) {
+  var prev = JSON.parse(localStorage.getItem("elm-test-ops"));
+  prev = prev ? prev : [];
+  if (op.opType == "Commit") {
+    localStorage.setItem("elm-test-ops", JSON.stringify([]));
+  } else {
+    localStorage.setItem("elm-test-ops", JSON.stringify(prev.concat(op)));
+  }
+}
+
+activateCard = function(uid) {
+  scrollTo(uid.toString());
+}
+
+
+/* Keyboard shortcuts */
+var shortcuts = [ 'mod+enter'
+                , 'enter'
+                , 'mod+j'
+                , 'mod+k'
+                , 'mod+l'
+                , 'mod+s'
+                , 'mod+z'
+                , 'mod+r'
+                ];
+
+var needOverride= [ 'mod+j'
+                  , 'mod+l'
+                  , 'mod+s'  
+                  , 'mod+r'
+                  ];
+                    
+Mousetrap.bind(shortcuts, function(e, s) {
+  gingko.ports.keyboard.send(s);
+
+  if(needOverride.includes(s)) {
+    return false;
+  }
+});
+
+
+
+/* React rendering of Commits graph */
+var commitsObject = [
+  {
+    "parents": [
+      "82aa2102c8291f56f8dfefce1dce40d8a0dd686b",
+      "175dfbbdbf8734069efaafced5a531dbf77c3a57"
+    ],
+    "sha": "5a7e04df76e21f9ba4a48098b6b26f19b51b99b1"
+  },
+  {
+    "parents": [
+      "90113cac59463df2e182e48444b8395658ebf840"
+    ],
+    "sha": "175dfbbdbf8734069efaafced5a531dbf77c3a57"
+  },
+  {
+    "parents": [
+      "82aa2102c8291f56f8dfefce1dce40d8a0dd686b"
+    ],
+    "sha": "90113cac59463df2e182e48444b8395658ebf840"
+  },
+  {
+    "parents": [
+      "395037182d9c34de2c26afc65b045ee875c93d25",
+      "3f3d28b62f46f40959e31d54dbe21941801e91e3"
+    ],
+    "sha": "82aa2102c8291f56f8dfefce1dce40d8a0dd686b"
+  },
+  {
+    "parents": [
+      "6c7fa94319d3ab727b5606ad2bd0a732e65e7012"
+    ],
+    "sha": "3f3d28b62f46f40959e31d54dbe21941801e91e3"
+  },
+  {
+    "parents": [
+      "639f3e8d021af55953b2d9ee55bf83a0193c7797"
+    ],
+    "sha": "6c7fa94319d3ab727b5606ad2bd0a732e65e7012"
+  },
+  {
+    "parents": [
+      "f22fae5a4b3bcdcd8583f9cd25da8fbacad8fcf3"
+    ],
+    "sha": "639f3e8d021af55953b2d9ee55bf83a0193c7797"
+  },
+  {
+    "parents": [
+      "3b4cb078964d91ed9e9d422bd61087c552a928a4"
+    ],
+    "sha": "f22fae5a4b3bcdcd8583f9cd25da8fbacad8fcf3"
+  },
+  {
+    "parents": [
+      "f6b061794ccd2f9a46daefe082cae360dea85a41"
+    ],
+    "sha": "3b4cb078964d91ed9e9d422bd61087c552a928a4"
+  },
+  {
+    "parents": [
+      "2e00fd89a9c551e51d3f7a44044b4d29ed33bbd2"
+    ],
+    "sha": "f6b061794ccd2f9a46daefe082cae360dea85a41"
+  },
+  {
+    "parents": [
+      "395037182d9c34de2c26afc65b045ee875c93d25"
+    ],
+    "sha": "2e00fd89a9c551e51d3f7a44044b4d29ed33bbd2"
+  },
+  {
+    "parents": [
+      "4cde659b0b50e3ffe4ef73def1a077d47c241b44"
+    ],
+    "sha": "395037182d9c34de2c26afc65b045ee875c93d25"
+  },
+  {
+    "parents": [
+      "8e0b15259fbb7dd339b9aca646501b4203973874"
+    ],
+    "sha": "4cde659b0b50e3ffe4ef73def1a077d47c241b44"
+  },
+  {
+    "parents": [
+      "acaf8ad845d28fb9b4119bcfc1500c9ce1a903b5"
+    ],
+    "sha": "8e0b15259fbb7dd339b9aca646501b4203973874"
+  },
+  {
+    "parents": [
+      "4335d3ab62d8b5531508a27e622ad186d86bcdc7"
+    ],
+    "sha": "acaf8ad845d28fb9b4119bcfc1500c9ce1a903b5"
+  },
+  {
+    "parents": [
+      "cd10df3588ca6858af0201c28b58a8259289e3f1"
+    ],
+    "sha": "4335d3ab62d8b5531508a27e622ad186d86bcdc7"
+  },
+  {
+    "parents": [
+      "496f8a4e3aaa53630c573076a9d988e020cbf1ca"
+    ],
+    "sha": "cd10df3588ca6858af0201c28b58a8259289e3f1"
+  },
+  {
+    "parents": [
+      "ee9f4af79ff6fab50f7ebdf823f06dfdbc000b24"
+    ],
+    "sha": "496f8a4e3aaa53630c573076a9d988e020cbf1ca"
+  },
+  {
+    "parents": [
+      "4b247b9f19b0b345755aaae1914a280e66d09a0c"
+    ],
+    "sha": "ee9f4af79ff6fab50f7ebdf823f06dfdbc000b24"
+  },
+  {
+    "parents": [
+      "2cb54f702fa5f0ffd2435dbae7b05d52f8124c1e"
+    ],
+    "sha": "4b247b9f19b0b345755aaae1914a280e66d09a0c"
+  },
+  {
+    "parents": [
+      "69d9c1b99fa70108614a453a91c57c2945261dfa"
+    ],
+    "sha": "2cb54f702fa5f0ffd2435dbae7b05d52f8124c1e"
+  },
+  {
+    "parents": [
+      "503867bfcf9b5b615909225af2410e8225c8a6a4"
+    ],
+    "sha": "69d9c1b99fa70108614a453a91c57c2945261dfa"
+  },
+  {
+    "parents": [
+      "d62b312eb28f396ff67afecdd68ca9bcf4adf722"
+    ],
+    "sha": "503867bfcf9b5b615909225af2410e8225c8a6a4"
+  },
+  {
+    "parents": [
+      "071e7bde5b16832be2a2e48f1f5c91733644adf5"
+    ],
+    "sha": "d62b312eb28f396ff67afecdd68ca9bcf4adf722"
+  },
+  {
+    "parents": [
+      "e72dd3608da184c8760d011bd4320b902f5fdc0b"
+    ],
+    "sha": "071e7bde5b16832be2a2e48f1f5c91733644adf5"
+  },
+  {
+    "parents": [
+      "0e2a0bfb87ae21c79518a6c6dc6aa9aee0bf6dfb"
+    ],
+    "sha": "e72dd3608da184c8760d011bd4320b902f5fdc0b"
+  },
+  {
+    "parents": [
+      "e41d31976f002cee56a917dde2c1c75615e39d63"
+    ],
+    "sha": "0e2a0bfb87ae21c79518a6c6dc6aa9aee0bf6dfb"
+  },
+  {
+    "parents": [
+      "96cd2f670d5829716a9a98d543d063f4fe304026"
+    ],
+    "sha": "e41d31976f002cee56a917dde2c1c75615e39d63"
+  },
+  {
+    "parents": [
+      "7d9b3577930547e3549f630cf21160fdbcaea87e"
+    ],
+    "sha": "96cd2f670d5829716a9a98d543d063f4fe304026"
+  },
+  {
+    "parents": [
+      "876ba955da6788d60180261ec884b34fc73e574c"
+    ],
+    "sha": "7d9b3577930547e3549f630cf21160fdbcaea87e"
+  },
+  {
+    "parents": [
+      "cc74c787c3e6f174700fac0f146acf67fe4233f4"
+    ],
+    "sha": "876ba955da6788d60180261ec884b34fc73e574c"
+  },
+  {
+    "parents": [
+      "50a5dae8710e0d7cd5aaa9bd240ff2ac2135f463"
+    ],
+    "sha": "cc74c787c3e6f174700fac0f146acf67fe4233f4"
+  },
+  {
+    "parents": [
+      "ac7dcd4513d30a970d3576be4492f82815a5964b"
+    ],
+    "sha": "50a5dae8710e0d7cd5aaa9bd240ff2ac2135f463"
+  },
+  {
+    "parents": [],
+    "sha": "ac7dcd4513d30a970d3576be4492f82815a5964b"
+  }
+]
+
+function handleGraphClick(sha) {
+  alert(sha);
+  render();
+}
+
+function render() {
+  ReactDOM.render(
+    React.createElement( CommitsGraph
+      , { commits: commitsObject
+        , onClick: handleGraphClick
+        , selected: "5a7e04df76e21f9ba4a48098b6b26f19b51b99b1"
+        })
+  , document.getElementById('graph')
+  );
+}
+
---
+++ dist/react-commits-graph.js
@@ -0,0 +1,378 @@
+/*
+Generate preformatted data of commits graph.
+ */
+var Node, generateGraphData, remove;
+
+generateGraphData = function(commits) {
+
+  /*
+  Generate graph data.
+  
+  :param commits: a list of commit, which should have
+    `sha`, `parents` properties.
+  :returns: data nodes, a json list of
+    [
+      sha,
+      [offset, branch], //dot
+      [
+      [from, to, branch],  // route 1
+      [from, to, branch],  // route 2
+      [from, to, branch],
+      ]  // routes
+    ],  // node
+   */
+  var b, branch, branchIndex, branches, commit, getBranch, i, j, k, l, len, len1, len2, len3, len4, m, n, node, nodes, numParents, offset, otherBranch, ref, ref1, reserve, routes;
+  nodes = [];
+  branchIndex = [0];
+  reserve = [];
+  branches = {};
+  getBranch = function(sha) {
+    if (branches[sha] == null) {
+      branches[sha] = branchIndex[0];
+      reserve.push(branchIndex[0]);
+      branchIndex[0]++;
+    }
+    return branches[sha];
+  };
+  for (j = 0, len = commits.length; j < len; j++) {
+    commit = commits[j];
+    branch = getBranch(commit.sha);
+    numParents = commit.parents.length;
+    offset = reserve.indexOf(branch);
+    routes = [];
+    if (numParents === 1) {
+      if (branches[commit.parents[0]] != null) {
+        ref = reserve.slice(offset + 1);
+        for (i = k = 0, len1 = ref.length; k < len1; i = ++k) {
+          b = ref[i];
+          routes.push([i + offset + 1, i + offset + 1 - 1, b]);
+        }
+        ref1 = reserve.slice(0, offset);
+        for (i = l = 0, len2 = ref1.length; l < len2; i = ++l) {
+          b = ref1[i];
+          routes.push([i, i, b]);
+        }
+        remove(reserve, branch);
+        routes.push([offset, reserve.indexOf(branches[commit.parents[0]]), branch]);
+      } else {
+        for (i = m = 0, len3 = reserve.length; m < len3; i = ++m) {
+          b = reserve[i];
+          routes.push([i, i, b]);
+        }
+        branches[commit.parents[0]] = branch;
+      }
+    } else if (numParents === 2) {
+      branches[commit.parents[0]] = branch;
+      for (i = n = 0, len4 = reserve.length; n < len4; i = ++n) {
+        b = reserve[i];
+        routes.push([i, i, b]);
+      }
+      otherBranch = getBranch(commit.parents[1]);
+      routes.push([offset, reserve.indexOf(otherBranch), otherBranch]);
+    }
+    node = Node(commit.sha, offset, branch, routes);
+    nodes.push(node);
+  }
+  return nodes;
+};
+
+remove = function(list, item) {
+  list.splice(list.indexOf(item), 1);
+  return list;
+};
+
+Node = function(sha, offset, branch, routes) {
+  return [sha, [offset, branch], routes];
+};
+
+SVGPathData = (function() {
+  function SVGPathData() {
+    this.commands = [];
+  }
+
+  SVGPathData.prototype.toString = function() {
+    return this.commands.join(' ');
+  };
+
+  SVGPathData.prototype.moveTo = function(x, y) {
+    return this.commands.push("M " + x + "," + y);
+  };
+
+  SVGPathData.prototype.lineTo = function(x, y) {
+    return this.commands.push("L " + x + "," + y);
+  };
+
+  SVGPathData.prototype.closePath = function(x, y) {
+    return this.commands.push("Z");
+  };
+
+  SVGPathData.prototype.bezierCurveTo = function(cp1x, cp1y, cp2x, cp2y, x, y) {
+    return this.commands.push("C " + cp1x + ", " + cp1y + ", " + cp2x + ", " + cp2y + ", " + x + ", " + y);
+  };
+
+  SVGPathData.prototype.quadraticCurveTo = function(cp1x, cp1y, x, y) {
+    return this.commands.push("Q " + cp1x + ", " + cp1y + ", " + x + ", " + y);
+  };
+
+  return SVGPathData;
+
+})();
+
+var COLOURS, CommitsGraphMixin, React, SVGPathData, branchCount, classSet, distance, generateGraphData, getColour,
+  slice = [].slice;
+
+COLOURS = ["#e11d21", "#fbca04", "#009800", "#006b75", "#207de5", "#0052cc", "#5319e7", "#f7c6c7", "#fad8c7", "#fef2c0", "#bfe5bf", "#c7def8", "#bfdadc", "#bfd4f2", "#d4c5f9", "#cccccc", "#84b6eb", "#e6e6e6", "#ffffff", "#cc317c"];
+
+classSet = function() {
+  var classes;
+  classes = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+  return classes.filter(Boolean).join(' ');
+};
+
+getColour = function(branch) {
+  var n;
+  n = COLOURS.length;
+  return COLOURS[branch % n];
+};
+
+branchCount = function(data) {
+  var i, j, maxBranch;
+  maxBranch = -1;
+  i = 0;
+  while (i < data.length) {
+    j = 0;
+    while (j < data[i][2].length) {
+      if (maxBranch < data[i][2][j][0] || maxBranch < data[i][2][j][1]) {
+        maxBranch = Math.max.apply(Math, [data[i][2][j][0], data[i][2][j][1]]);
+      }
+      j++;
+    }
+    i++;
+  }
+  return maxBranch + 1;
+};
+
+distance = function(point1, point2) {
+  var xs, ys;
+  xs = 0;
+  ys = 0;
+  xs = point2.x - point1.x;
+  xs = xs * xs;
+  ys = point2.y - point1.y;
+  ys = ys * ys;
+  return Math.sqrt(xs + ys);
+};
+
+CommitsGraphMixin = {
+  getDefaultProps: function() {
+    return {
+      y_step: 20,
+      x_step: 20,
+      dotRadius: 3,
+      lineWidth: 2,
+      selected: null,
+      mirror: false,
+      unstyled: false
+    };
+  },
+  componentWillReceiveProps: function() {
+    this.graphData = null;
+    return this.branchCount = null;
+  },
+  cursorPoint: function(e) {
+    var svg, svgPoint;
+    svg = this.getDOMNode();
+    svgPoint = svg.createSVGPoint();
+    svgPoint.x = e.clientX;
+    svgPoint.y = e.clientY;
+    return svgPoint.matrixTransform(svg.getScreenCTM().inverse());
+  },
+  handleClick: function(e) {
+    var base, closestCommit, commit, commitDistance, cursorLoc, k, len, ref, smallestDistance;
+    cursorLoc = this.cursorPoint(e);
+    smallestDistance = 2e308;
+    closestCommit = null;
+    ref = this.renderedCommitsPositions;
+    for (k = 0, len = ref.length; k < len; k++) {
+      commit = ref[k];
+      commitDistance = distance(cursorLoc, commit);
+      if (commitDistance < smallestDistance) {
+        smallestDistance = commitDistance;
+        closestCommit = commit;
+      }
+    }
+    return typeof (base = this.props).onClick === "function" ? base.onClick(closestCommit.sha) : void 0;
+  },
+  getGraphData: function() {
+    return this.graphData || (this.graphData = generateGraphData(this.props.commits));
+  },
+  getBranchCount: function() {
+    return this.branchCount || (this.branchCount = branchCount(this.getGraphData()));
+  },
+  getWidth: function() {
+    if (this.props.width != null) {
+      return this.props.width;
+    }
+    return this.getContentWidth();
+  },
+  getContentWidth: function() {
+    return (this.getBranchCount() + 0.5) * this.props.x_step;
+  },
+  getHeight: function() {
+    if (this.props.height != null) {
+      return this.props.height;
+    }
+    return this.getContentHeight();
+  },
+  getContentHeight: function() {
+    return (this.getGraphData().length + 2) * this.props.y_step;
+  },
+  getInvert: function() {
+    if (this.props.mirror) {
+      return 0 - this.props.width;
+    } else {
+      return 0;
+    }
+  },
+  getOffset: function() {
+    return this.getWidth() / 2 - this.getContentWidth() / 2;
+  },
+  renderRouteNode: function(svgPathDataAttribute, branch) {
+    var classes, colour, style;
+    if (!this.props.unstyled) {
+      colour = getColour(branch);
+      style = {
+        'stroke': colour,
+        'stroke-width': this.props.lineWidth,
+        'fill': 'none'
+      };
+    }
+    classes = "commits-graph-branch-" + branch;
+    return React.DOM.path({
+      d: svgPathDataAttribute,
+      style: style,
+      className: classes
+    });
+  },
+  renderRoute: function(commit_idx, arg) {
+    var branch, from, from_x, from_y, invert, offset, ref, svgPath, to, to_x, to_y, x_step, y_step;
+    from = arg[0], to = arg[1], branch = arg[2];
+    ref = this.props, x_step = ref.x_step, y_step = ref.y_step;
+    offset = this.getOffset();
+    invert = this.getInvert();
+    svgPath = new SVGPathData;
+    from_x = offset + invert + (from + 1) * x_step;
+    from_y = (commit_idx + 0.5) * y_step;
+    to_x = offset + invert + (to + 1) * x_step;
+    to_y = (commit_idx + 0.5 + 1) * y_step;
+    svgPath.moveTo(from_x, from_y);
+    if (from_x === to_x) {
+      svgPath.lineTo(to_x, to_y);
+    } else {
+      svgPath.bezierCurveTo(from_x - x_step / 4, from_y + y_step / 3 * 2, to_x + x_step / 4, to_y - y_step / 3 * 2, to_x, to_y);
+    }
+    return this.renderRouteNode(svgPath.toString(), branch);
+  },
+  renderCommitNode: function(x, y, sha, dot_branch) {
+    var classes, colour, radius, selectedClass, strokeColour, strokeWidth, style;
+    radius = this.props.dotRadius;
+    if (!this.props.unstyled) {
+      colour = getColour(dot_branch);
+      if (sha === this.props.selected) {
+        strokeColour = '#000';
+        strokeWidth = 2;
+      } else {
+        strokeColour = colour;
+        strokeWidth = 1;
+      }
+      style = {
+        'stroke': strokeColour,
+        'stroke-width': strokeWidth,
+        'fill': colour
+      };
+    }
+    if (this.props.selected) {
+      selectedClass = 'selected';
+    }
+    classes = classSet("commits-graph-branch-" + dot_branch, selectedClass);
+    return React.DOM.circle({
+      cx: x,
+      cy: y,
+      r: radius,
+      style: style,
+      onClick: this.handleClick,
+      'data-sha': sha,
+      className: classes
+    });
+  },
+  renderCommit: function(idx, arg) {
+    var commitNode, dot, dot_branch, dot_offset, index, invert, offset, ref, route, routeNodes, routes_data, sha, x, x_step, y, y_step;
+    sha = arg[0], dot = arg[1], routes_data = arg[2];
+    dot_offset = dot[0], dot_branch = dot[1];
+    ref = this.props, x_step = ref.x_step, y_step = ref.y_step;
+    offset = this.getOffset();
+    invert = this.getInvert();
+    x = offset + invert + (dot_offset + 1) * x_step;
+    y = (idx + 0.5) * y_step;
+    commitNode = this.renderCommitNode(x, y, sha, dot_branch);
+    routeNodes = (function() {
+      var k, len, results;
+      results = [];
+      for (index = k = 0, len = routes_data.length; k < len; index = ++k) {
+        route = routes_data[index];
+        results.push(this.renderRoute(idx, route));
+      }
+      return results;
+    }).call(this);
+    this.renderedCommitsPositions.push({
+      x: x,
+      y: y,
+      sha: sha
+    });
+    return [commitNode, routeNodes];
+  },
+  renderGraph: function() {
+    var allCommitNodes, allRouteNodes, children, commit, commitNode, height, index, k, len, ref, ref1, routeNodes, style, svgProps, width;
+    this.renderedCommitsPositions = [];
+    allCommitNodes = [];
+    allRouteNodes = [];
+    ref = this.getGraphData();
+    for (index = k = 0, len = ref.length; k < len; index = ++k) {
+      commit = ref[index];
+      ref1 = this.renderCommit(index, commit), commitNode = ref1[0], routeNodes = ref1[1];
+      allCommitNodes.push(commitNode);
+      allRouteNodes = allRouteNodes.concat(routeNodes);
+    }
+    children = [].concat(allRouteNodes, allCommitNodes);
+    height = this.getHeight();
+    width = this.getWidth();
+    if (!this.props.unstyled) {
+      style = {
+        height: height,
+        width: width,
+        cursor: 'pointer'
+      };
+    }
+    svgProps = {
+      height: height,
+      width: width,
+      style: style,
+      children: children
+    };
+    return React.DOM.svg({
+      onClick: this.handleClick,
+      height: height,
+      width: width,
+      style: style,
+      children: children
+    });
+  }
+};
+
+CommitsGraph = React.createClass({
+  displayName: 'CommitsGraph',
+  mixins: [CommitsGraphMixin],
+  render: function() {
+    return this.renderGraph();
+  }
+});
