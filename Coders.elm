137f2b397e84bed94c140f4260fb57d44b24e5b6
Adriano Ferrari <adriano.ferrari@gmail.com>
2016-10-10 15:59:33 -0400

    Added Decoder for State & all its dependencies.

--- Coders.elm
+++ Coders.elm
@@ -46,9 +46,67 @@ opToValue op =
 
 -- DECODERS
 
+stateDecoder : Decoder State
+stateDecoder =
+  Json.object3 State
+    ("objects" := objectsDecoder)
+    ("commit" := string)
+    ("viewState" := viewStateDecoder)
+
+
+objectsDecoder : Decoder Objects
+objectsDecoder =
+  Json.object4 Objects
+    ("contents" := Json.list contentDecoder)
+    ("nodes" := Json.list nodeDecoder)
+    ("commits" := Json.list commitDecoder)
+    ("operations" := Json.array opDecoder)
+
+
+contentDecoder : Decoder Content
+contentDecoder =
+  Json.object3 Content
+    ("id" := string)
+    ("contentType" := string)
+    ("content" := string)
+
+
+nodeDecoder : Decoder Node
+nodeDecoder =
+  Json.object3 Node
+    ("id" := string)
+    ("contentId" := string)
+    ("childrenIds" := Json.list string)
+
+
+commitDecoder : Decoder Commit
+commitDecoder =
+  Json.object7 Commit
+    ("id" := string)
+    ("rootNode" := string)
+    ("timestamp" := int)
+    ("authors" := Json.list string)
+    ("committer" := string)
+    ("parents" := Json.list string)
+    ("message" := string)
+
+
+viewStateDecoder : Decoder ViewState
+viewStateDecoder =
+  Json.object6 ViewState
+    ("active" := string)
+    ("activePast" := list string)
+    ("activeFuture" := list string)
+    ("descendants" := list string)
+    ( maybe ("editing" := string))
+    ("field" := string)
+    
+  
+
 opDecoder : Decoder Op
 opDecoder =
   ("type" := string) `andThen` opInfo
+
 
 opInfo : String -> Decoder Op
 opInfo tag =
