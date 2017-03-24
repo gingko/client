123e82cfe77a126c1824e98b0a9c0b1c0d04b28c
Adriano Ferrari <adriano.ferrari@gmail.com>
2017-03-24 10:27:58 -0400

    Added 'shared.js' for shared Javascript code.

--- src/web/main.js
+++ src/web/main.js
@@ -2,6 +2,8 @@ var jQuery = require('jquery')
 var _ = require('underscore')
 var autosize = require('textarea-autosize')
 const url = require('url')
+
+const shared = require('../shared/shared')
 window.Elm = require('../elm/Main')
 
 
@@ -145,8 +147,8 @@ gingko.ports.message.subscribe(function(msg) {
 })
 
 gingko.ports.activateCards.subscribe(actives => {
-  scrollHorizontal(actives[0])
-  scrollColumns(actives[1])
+  shared.scrollHorizontal(actives[0])
+  shared.scrollColumns(actives[1])
 })
 
 gingko.ports.attemptUpdate.subscribe(id => {
@@ -366,66 +368,6 @@ undoRedoMenuState = (past, future) => {
 
 /* === DOM manipulation === */
 
-var setTextarea = (m, f) => {
-  if(m.viewState.editing !== null && f !== null) {
-    var textarea = document.getElementById('card-edit-'+m.viewState.editing)
-    textarea.value = f
-  }
-}
-
-var scrollHorizontal = colIdx => {
-  lastColumnIdx = colIdx
-  _.delay(scrollHorizTo, 20, colIdx)
-}
-
-var scrollColumns = centerlineIds => {
-  lastCenterline = centerlineIds
-  centerlineIds.map(function(c, i){
-    var centerIdx = Math.round(c.length/2) - 1
-    _.delay(scrollTo, 20, c[centerIdx], i)
-  })
-}
-
-var scrollTo = function(cid, colIdx) {
-  var card = document.getElementById('card-' + cid.toString());
-  var col = document.getElementsByClassName('column')[colIdx+1]
-  if (card == null) {
-    console.log('scroll error: not found',cid)
-    return;
-  }
-  var rect = card.getBoundingClientRect();
-
-  TweenMax.to(col, 0.35,
-    { scrollTop: col.scrollTop + ((rect.top + rect.height*0.5) - col.offsetHeight*0.5)
-    , ease: Power2.easeInOut
-    });
-}
-
-var scrollHorizTo = function(colIdx) {
-  var col = document.getElementsByClassName('column')[colIdx+1]
-  var appEl = document.getElementById('app');
-  if (col == null) {
-    console.log('scroll horiz error: not found', colIdx)
-    return;
-  }
-  var rect = col.getBoundingClientRect();
-  if (rect.width >= appEl.offsetWidth) {
-    TweenMax.to(appEl, 0.50,
-      { scrollLeft: appEl.scrollLeft + rect.left
-      , ease: Power2.easeInOut
-      });
-  } else if (rect.left < 100) {
-    TweenMax.to(appEl, 0.50,
-      { scrollLeft: appEl.scrollLeft - 100 + rect.left
-      , ease: Power2.easeInOut
-      });
-  } else if (rect.right > appEl.offsetWidth - 100) {
-    TweenMax.to(appEl, 0.50,
-      { scrollLeft: appEl.scrollLeft + 100 + rect.right - appEl.offsetWidth 
-      , ease: Power2.easeInOut
-      });
-  }
-}
 
 
 var observer = new MutationObserver(function(mutations) {
