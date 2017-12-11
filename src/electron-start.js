34dcff1107021ade8847e09867e12b10a7446cac
Adriano Ferrari <adriano.ferrari@gmail.com>
2017-12-11 16:52:08 -0500

    Added dblsqd autoUpdater again, on *testing* channel.

--- src/electron-start.js
+++ src/electron-start.js
@@ -1,4 +1,5 @@
 const {app, BrowserWindow, dialog, Menu, ipcMain, shell} = require('electron')
+const {autoUpdater} = require('dblsqd-electron')
 const fs = require('mz/fs')
 const path = require('path')
 const sha1 = require('sha1')
@@ -12,8 +13,12 @@ const hiddenStore = new Store({name: "kernel", encryptionKey: "79df64f73eab9bc0d
 const userStore = new Store({name: "config"})
 
 
-if(require('electron-squirrel-startup')) app.quit()
-
+//Enable DBLSQD autoUpdater
+const dblsqdBaseUrl = 'https://feeds.dblsqd.com/IEsVYK1_Te2BZIyJWhcelw'
+const dblsqdChannel = 'testing'
+const updater = new autoUpdater(dblsqdBaseUrl, dblsqdChannel, {
+  icon: `${__dirname}/static/leaf128.png`
+})
 
 
 
