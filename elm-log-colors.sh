4b6cc4ab2c87a4c18ceb1f9e36500008568f2358
Adriano Ferrari <adriano.ferrari@gmail.com>
2017-12-19 14:14:09 -0500

    Added unbuffer installation commands to elm-log-colors.sh .

--- elm-log-colors.sh
+++ elm-log-colors.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 # Fakes elm-make into printing colors when
-# run by elm-webpack-loader
+# run by elm-webpack-loader.
+
+# Requires unbuffer to be installed:
+
+# On linux/ubuntu:
+# $ sudo apt-get install expect-dev
+
+# On macOS:
+# $ brew install expect
 unbuffer node_modules/.bin/elm-make $@
