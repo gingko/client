077fa83a4d732fee405aaa1875e6b7204bae104e
Adriano Ferrari <adriano.ferrari@gmail.com>
2018-04-19 12:56:52 -0400

    Sending from Elm to JS using unionTypeToString, instead of hardcoded strings.

--- src/elm/Ports.elm
+++ src/elm/Ports.elm
@@ -15,10 +15,7 @@ sendOut info =
   in
   case info of
     Alert str ->
-      infoForOutside
-        { tag = "Alert"
-        , data = string str
-        }
+      dataToSend ( string str )
 
     ChangeTitle filepath_ changed ->
       dataToSend ( list [ maybeToValue string filepath_ , bool changed ] )
@@ -46,28 +43,16 @@ sendOut info =
             |> List.map list
             |> list
       in
-      infoForOutside
-        { tag = "ActivateCards"
-        , data = tripleToValue string int listListStringToValue ( cardId, col, cardIds )
-        }
+      dataToSend ( tripleToValue string int listListStringToValue ( cardId, col, cardIds ) )
 
     TextSurround id str ->
-      infoForOutside
-        { tag = "TextSurround"
-        , data = list [ string id, string str ]
-        }
+      dataToSend ( list [ string id, string str ] )
 
     ConfirmCancelCard id origContent ->
-      infoForOutside
-        { tag = "ConfirmCancelCard"
-        , data = list [ string id, string origContent ]
-        }
+      dataToSend ( list [ string id, string origContent ] )
 
     ColumnNumberChange cols ->
-      infoForOutside
-        { tag = "ColumnNumberChange"
-        , data = int cols
-        }
+      dataToSend ( int cols )
 
     OpenDialog filepath_ ->
       dataToSend ( maybeToValue string filepath_ )
@@ -79,17 +64,13 @@ sendOut info =
       dataToSend ( maybeToValue string filepath_ )
 
     ExportJSON tree ->
-      infoForOutside
-        { tag = "ExportJSON"
-        , data = treeToJSON tree
-        }
+      dataToSend ( treeToJSON tree )
 
     ExportTXT withRoot tree ->
-      infoForOutside
-        { tag = "ExportTXT"
-        , data = treeToMarkdown withRoot tree
-        }
+      dataToSend ( treeToMarkdown withRoot tree )
 
+    -- ExportTXTColumn is handled by 'ExportTXT' in JS
+    -- So we use the "ExportTXT" tag here, instead of `dataToSend`
     ExportTXTColumn col tree ->
       infoForOutside
         { tag = "ExportTXT"
@@ -104,19 +85,16 @@ sendOut info =
         }
 
     Push ->
-      tagOnly "Push"
+      dataToSend null
 
     Pull ->
-      tagOnly "Pull"
+      dataToSend null
 
     SaveToDB ( statusValue, objectsValue ) ->
       dataToSend ( list [ statusValue, objectsValue ] )
 
     SaveLocal tree ->
-      infoForOutside
-        { tag = "SaveLocal"
-        , data = treeToValue tree
-        }
+      dataToSend ( treeToValue tree )
 
     UpdateCommits ( objectsValue, head_ ) ->
       let
@@ -125,34 +103,21 @@ sendOut info =
             Just str -> string str
             Nothing -> null
       in
-      infoForOutside
-        { tag = "UpdateCommits"
-        , data = tupleToValue identity headToValue ( objectsValue, head_ )
-        }
+      dataToSend ( tupleToValue identity headToValue ( objectsValue, head_ ) )
 
     SetVideoModal isOpen ->
-      infoForOutside
-        { tag = "SetVideoModal"
-        , data = bool isOpen
-        }
+      dataToSend ( bool isOpen )
 
     SetShortcutTray isOpen ->
-      infoForOutside
-        { tag = "SetShortcutTray"
-        , data = bool isOpen
-        }
+      dataToSend ( bool isOpen )
 
     SocketSend collabState ->
-      infoForOutside
-        { tag = "SocketSend"
-        , data = collabStateToValue collabState
-        }
+      dataToSend ( collabStateToValue collabState )
 
     ConsoleLogRequested err ->
-      infoForOutside
-        { tag = "ConsoleLogRequested"
-        , data = string err
-        }
+      dataToSend ( string err )
+
+
 
 
 receiveMsg : (IncomingMsg -> msg) -> (String -> msg) -> Sub msg
@@ -271,9 +236,6 @@ receiveMsg tagger onError =
     )
 
 
-tagOnly : String -> Cmd msg
-tagOnly tag =
-  infoForOutside { tag = tag, data = null }
 
 
 encodeAndSend : OutgoingMsg -> Json.Encode.Value -> Cmd msg
