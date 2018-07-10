c28494d1488541fe135d0e16d5758b608cb7a22e
Adriano Ferrari <adriano.ferrari@gmail.com>
2018-07-10 14:08:19 -0400

    elm-format for TreeUtils.elm

--- src/elm/TreeUtils.elm
+++ src/elm/TreeUtils.elm
@@ -95,13 +95,14 @@ getColumn n tree =
 
 getColumnById : String -> Tree -> Maybe Column
 getColumnById id tree =
-  let
-    n = getDepth 0 tree id
-  in
-  getColumn n tree
+    let
+        n =
+            getDepth 0 tree id
+    in
+    getColumn n tree
 
 
-getTreeWithPosition : String -> Tree -> Maybe (Tree, String, Int)
+getTreeWithPosition : String -> Tree -> Maybe ( Tree, String, Int )
 getTreeWithPosition id tree =
     Maybe.map3
         (\t p i -> ( t, p, i ))
@@ -142,27 +143,30 @@ getNext id tree =
 
 getPrevNextInColumn : Int -> String -> Tree -> Maybe Tree
 getPrevNextInColumn shift id tree =
-  let
-    column_ = getColumnById id tree
-  in
-  case column_ of
-    Nothing -> Nothing
+    let
+        column_ =
+            getColumnById id tree
+    in
+    case column_ of
+        Nothing ->
+            Nothing
 
-    Just col ->
-      let
-        idx =
-          col
-            |> List.concat
-            |> List.map .id
-            |> ListExtra.elemIndex id
-      in
-      case idx of
-        Nothing -> Nothing
+        Just col ->
+            let
+                idx =
+                    col
+                        |> List.concat
+                        |> List.map .id
+                        |> ListExtra.elemIndex id
+            in
+            case idx of
+                Nothing ->
+                    Nothing
 
-        Just i ->
-          col
-            |> List.concat
-            |> ListExtra.getAt (i + shift)
+                Just i ->
+                    col
+                        |> List.concat
+                        |> ListExtra.getAt (i + shift)
 
 
 getPrevInColumn : String -> Tree -> Maybe Tree
@@ -174,28 +178,46 @@ getNextInColumn : String -> Tree -> Maybe Tree
 getNextInColumn id tree =
     getPrevNextInColumn 1 id tree
 
+
 getFirstInColumn : String -> Tree -> String
 getFirstInColumn id tree =
-  case getColumnById id tree of
-    Nothing -> id
-    Just c -> case c
-      |> List.concat
-      |> List.map .id
-      |> List.head of
-        Nothing -> id
-        Just firstId -> firstId
+    case getColumnById id tree of
+        Nothing ->
+            id
+
+        Just c ->
+            case
+                c
+                    |> List.concat
+                    |> List.map .id
+                    |> List.head
+            of
+                Nothing ->
+                    id
+
+                Just firstId ->
+                    firstId
+
 
 getLastInColumn : String -> Tree -> String
 getLastInColumn id tree =
-  case getColumnById id tree of
-    Nothing -> id
-    Just c -> case c
-      |> List.concat
-      |> List.map .id
-      |> List.reverse
-      |> List.head of
-        Nothing -> id
-        Just firstId -> firstId
+    case getColumnById id tree of
+        Nothing ->
+            id
+
+        Just c ->
+            case
+                c
+                    |> List.concat
+                    |> List.map .id
+                    |> List.reverse
+                    |> List.head
+            of
+                Nothing ->
+                    id
+
+                Just firstId ->
+                    firstId
 
 
 getContent : String -> Tree -> String
