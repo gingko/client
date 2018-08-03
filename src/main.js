ea7ddd7e63a2d9ceb011c81b2404b3af66cd3c12
Adriano Ferrari <adriano.ferrari@gmail.com>
2018-08-03 13:18:13 -0400

    Load both files and legacy folders from Home

--- src/main.js
+++ src/main.js
@@ -800,10 +800,15 @@ ipcMain.on('home:import-file', async (event) => {
 })
 
 
-ipcMain.on("home:load", (event, dbToLoad, docName) => {
-  let swapPath = path.join(app.getPath("userData"), dbToLoad);
-  createDocumentWindow(swapPath, null, { "name": docName })
-  winHome.close()
+ipcMain.on("home:load", async (event, dbToLoad, docName) => {
+  if (/^[a-f0-9]{40}$/i.test(dbToLoad)) {
+    const swapPath = path.join(app.getPath("userData"), dbToLoad);
+    createDocumentWindow(swapPath, null, { "name": docName });
+    winHome.close();
+  } else if (path.isAbsolute(dbToLoad) && fs.pathExistsSync(dbToLoad)) {
+    await openDocument(dbToLoad);
+    winHome.close();
+  }
 })
 
 
