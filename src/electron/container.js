c45f1ff86109b27c748e27d0b250546d485b8812
Adriano Ferrari <adriano.ferrari@gmail.com>
2020-06-01 06:09:50 -0400

    Fix "inability to export" bug
    Close #196.

--- src/electron/container.js
+++ src/electron/container.js
@@ -24,7 +24,7 @@ const getInitialDocState = () => {
 };
 
 
-const exportDocx = (data, defaultPath) => {
+const exportDocx = async (data, defaultPath) => {
   if (data && typeof data.replace === "function") {
     data = (process.platform === "win32") ? data.replace(/\n/g, "\r\n") : data;
   } else {
@@ -37,98 +37,98 @@ const exportDocx = (data, defaultPath) => {
     , filters:  [ {name: "Word Files", extensions: ["docx"]} ]
     };
 
-  dialog.showSaveDialog(options, function(filepath){
-    if(typeof filepath == "string"){
-      let tmpMarkdown = path.join(app.getPath("temp"), path.basename(filepath) + ".md");
+  let {filePath} = await dialog.showSaveDialog(options);
 
-      fs.writeFile(tmpMarkdown, data, (err) => {
-        if (err) throw new Error("export-docx writeFile failed");
+  if(typeof filePath == "string"){
+    let tmpMarkdown = path.join(app.getPath("temp"), path.basename(filePath) + ".md");
 
-        let pandocPath = path.join(__dirname, "/../../pandoc");
+    fs.writeFile(tmpMarkdown, data, (err) => {
+      if (err) throw new Error("export-docx writeFile failed");
 
-        // pandoc file is copied by electron-builder
-        // so we need to point to the src directory when running with `npm run electron`
-        // TODO : Fix this.
-        if (!app.isPackaged) {
-          switch (process.platform) {
-            case "linux":
-              pandocPath = path.join(__dirname, "/../../src/bin/linux/pandoc");
-              break;
+      let pandocPath = path.join(__dirname, "/../../pandoc");
 
-            case "win32":
-              pandocPath = path.join(__dirname, "/../../src/bin/win/pandoc.exe");
-              break;
+      // pandoc file is copied by electron-builder
+      // so we need to point to the src directory when running with `npm run electron`
+      // TODO : Fix this.
+      if (!app.isPackaged) {
+        switch (process.platform) {
+          case "linux":
+            pandocPath = path.join(__dirname, "/../../src/bin/linux/pandoc");
+            break;
 
-            case "darwin":
-              pandocPath = path.join(__dirname, "/../../src/bin/mac/pandoc");
-              break;
-          }
+          case "win32":
+            pandocPath = path.join(__dirname, "/../../src/bin/win/pandoc.exe");
+            break;
+
+          case "darwin":
+            pandocPath = path.join(__dirname, "/../../src/bin/mac/pandoc");
+            break;
         }
+      }
 
-        execFile( pandocPath
-          , [ tmpMarkdown
-            , "--from=gfm+hard_line_breaks"
-            , "--to=docx"
-            , `--output=${filepath}`
-            , "--verbose"
-            ]
-          , ( err, stdout, stderr) => {
+      execFile( pandocPath
+        , [ tmpMarkdown
+          , "--from=gfm+hard_line_breaks"
+          , "--to=docx"
+          , `--output=${filePath}`
+          , "--verbose"
+          ]
+        , ( err, stdout, stderr) => {
+            if (err) {
+              throw err;
+            }
+
+            fs.unlink(tmpMarkdown, (err) => {
               if (err) {
                 throw err;
               }
 
-              fs.unlink(tmpMarkdown, (err) => {
-                if (err) {
-                  throw err;
-                }
-
-                let exportSuccessNotification = new Notification("Export Suceeded", {
-                  body: "Saved as " + path.basename(filepath)
-                });
-
-                exportSuccessNotification.onclick = () => {
-                  shell.openItem(filepath);
-                };
+              let exportSuccessNotification = new Notification("Export Suceeded", {
+                body: "Saved as " + path.basename(filePath)
               });
-          });
-      });
-    }
-  });
+
+              exportSuccessNotification.onclick = () => {
+                shell.openItem(filePath);
+              };
+            });
+        });
+    });
+  }
 };
 
 
-const exportJson = (data, defaultPath) => {
+const exportJson = async (data, defaultPath) => {
+  var options =
+    { title: "Export JSON"
+    , defaultPath: defaultPath ? defaultPath.replace(".gko", "") : path.join(app.getPath("documents"),"Untitled.json")
+    , filters:  [ {name: "Gingko JSON (*.json)", extensions: ["json"]}
+                , {name: "All Files", extensions: ["*"]}
+                ]
+    };
+
+  let {filePath} = await dialog.showSaveDialog(options);
+
   return new Promise(
     (resolve, reject) => {
-      var options =
-        { title: "Export JSON"
-        , defaultPath: defaultPath ? defaultPath.replace(".gko", "") : path.join(app.getPath("documents"),"Untitled.json")
-        , filters:  [ {name: "Gingko JSON (*.json)", extensions: ["json"]}
-                    , {name: "All Files", extensions: ["*"]}
-                    ]
-        };
-
-      dialog.showSaveDialog(options, function(filepath){
-        if(filepath){
-          fs.writeFile(filepath, JSON.stringify(data, undefined, 2), (err) => {
-            if (err) {
-              reject(new Error("export-json writeFile failed"));
-              return;
-            }
-            resolve(data);
-          });
-        } else {
-          reject(new Error("no export path chosen"));
-          return;
-        }
-      });
+      if(filePath){
+        fs.writeFile(filePath, JSON.stringify(data, undefined, 2), (err) => {
+          if (err) {
+            reject(new Error("export-json writeFile failed"));
+            return;
+          }
+          resolve(data);
+        });
+      } else {
+        reject(new Error("no export path chosen"));
+        return;
+      }
     }
   );
 };
 
 const exportTxt = (data, defaultPath) => {
   return new Promise(
-    (resolve, reject) => {
+    async (resolve, reject) => {
       if (data && typeof data.replace === "function") {
         data = (process.platform === "win32") ? data.replace(/\n/g, "\r\n") : data;
       } else {
@@ -166,14 +166,14 @@ const exportTxt = (data, defaultPath) => {
           };
 
 
-        dialog.showSaveDialog(options, function(filepath){
-          if(filepath){
-            saveFile(filepath);
-          } else {
-            reject(new Error("no export path chosen"));
-            return;
-          }
-        });
+        let {filePath} = await dialog.showSaveDialog(options);
+
+        if(filePath){
+          saveFile(filePath);
+        } else {
+          reject(new Error("no export path chosen"));
+          return;
+        }
       }
     }
   );
