d0bf6a25ed5d57dcdd16e395330114b242456a80
Adriano Ferrari <adriano.ferrari@gmail.com>
2020-12-08 11:42:57 -0500

    Add test for redirecting to Empty page on deleting last tree

--- cypress/integration/empty.js
+++ cypress/integration/empty.js
@@ -10,14 +10,40 @@ describe('Empty State', () => {
     cy.visit(config.TEST_SERVER)
   })
 
-  it('Should show Empty page', ()=> {
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce('AuthSession')
+  })
+
+  it('Shows Empty page', ()=> {
     cy.url().should('eq', config.TEST_SERVER + '/')
     cy.contains("You don't have any documents. Create one here:")
   })
 
-  it('Should show Template Selector on clicking "New"', () => {
+  it('Goes to newly created tree', () => {
     cy.get('#new-button').click()
 
     cy.get('#templates-block')
+      .get('#template-new')
+      .click()
+
+    cy.url().should('match', /\/[a-zA-Z0-9]{5}/)
+
+    cy.writeInCard('words')
+    cy.shortcut('{ctrl}{enter}')
+
+    cy.contains('Synced')
+  })
+
+  it('Goes back to Empty page on deletion', () => {
+    cy.get('#file-button').click()
+
+    cy.get('#sidebar-menu .sidebar-document-item')
+      .first()
+      .rightclick()
+
+    cy.contains('Delete Tree')
+      .click()
+
+    cy.url().should('equal', config.TEST_SERVER + '/')
   })
 })
