ac74c89414e194ea7832fdc6b0e90fd78fe1f793
Adriano Ferrari <adriano.ferrari@gmail.com>
2020-12-11 12:31:04 -0500

    Remove Debug.log from Session

--- src/elm/Session.elm
+++ src/elm/Session.elm
@@ -187,7 +187,7 @@ updateDocuments : DocList.Model -> Session -> Session
 updateDocuments docList session =
     case session of
         LoggedIn sessionData data ->
-            LoggedIn sessionData { data | documents = DocList.update docList data.documents |> Debug.log "new docs" }
+            LoggedIn sessionData { data | documents = DocList.update docList data.documents }
 
         Guest _ _ ->
             session
