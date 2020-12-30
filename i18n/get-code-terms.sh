b34ba5acb371495926ff4c240586b80038b7c80c
Adriano Ferrari <adriano.ferrari@gmail.com>
2020-12-29 23:53:03 -0500

    Finish Elm-only get-code-terms.sh

--- i18n/get-code-terms.sh
+++ i18n/get-code-terms.sh
@@ -8,13 +8,4 @@ s/\s{20}\{ en = "(.*)"/msgid "\1"\nmsgstr ""/
 s/\s{20}\{ en = .*"(.*)" "(.*)"/msgid "\1"\nmsgid_plural "\2"\nmsgstr[0] ""\nmsgstr[1] ""/
 /(^#:|msgid|msgstr)/!d
 s/^#:(.*)/\n#:\1/
-' ../src/elm/Translation.elm > elm.po
-
-# translation.js to all-language .po
-sed -E '
-s/\s{4}(\S*) :/#:JS:\1/
-s/\s{2}, (\S*) :/#:JS:\1/
-s/\s{6}\{ en : "(.*)"/msgid "\1"\nmsgstr ""/
-/(^#:|msgid|msgstr)/!d
-s/^#:(.*)/\n#:\1/
-' ../src/shared/translation.js > js.po
+' ../src/elm/Translation.elm > terms.po
