64d23611575df8587de1e9dc7fa6d9d37839e0fb
Adriano Ferrari <adriano.ferrari@gmail.com>
2021-01-15 16:03:28 -0500

    Add INR currency to config

--- config-example.js
+++ config-example.js
@@ -11,16 +11,28 @@ module.exports = {
   STRIPE_PUBLIC_KEY: "pk_test_123412341234",
   PRICE_DATA :
     { USD :
-        { monthly :
-          { regular: "price_id_12341234"
-          , discount: "price_id_234234"
-          , bonus: "price_id_542543"
-          }
-        , yearly :
-          { regular: "price_id_1234"
-          , discount: "price_id_1234"
-          , bonus: "price_id_1234"
-          }
+      { monthly :
+        { regular: "price_id_12341234"
+        , discount: "price_id_234234"
+        , bonus: "price_id_542543"
         }
+      , yearly :
+        { regular: "price_id_1234"
+        , discount: "price_id_1234"
+        , bonus: "price_id_1234"
+        }
+      }
+    , INR :
+      { monthly :
+        { regular: "price_0I9sEibEm7WWhuzY1mBaArgx"
+        , discount: "price_0I9sEibEm7WWhuzYFsufwbEc"
+        , bonus: "price_0I9sEjbEm7WWhuzY6Z88IprN"
+        }
+      , yearly :
+        { regular: "price_0I9sEjbEm7WWhuzYeqXwYr59"
+        , discount: "price_0I9sEjbEm7WWhuzYaoRXvNL7"
+        , bonus: "price_0I9sEjbEm7WWhuzYCPhDUSI7"
+        }
+      }
     }
 };
