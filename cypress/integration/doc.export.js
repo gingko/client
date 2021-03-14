31452d0886a50616257c988c04b6c9609391cd05
Adriano Ferrari <adriano.ferrari@gmail.com>
2021-03-14 10:56:31 -0400

    Complete tests for 'Current & Subtree' export option

--- cypress/integration/doc.export.js
+++ cypress/integration/doc.export.js
@@ -31,6 +31,7 @@ describe('Document Exporting', () => {
 
     describe('Whole Tree', () => {
       describe('Word format', () => {
+        cy.get('#export-word').click()
         cy.get('#export-preview', {log: false})
           .should('contain.html', '<h1 id="act-i">Act I</h1>')
           .should('contain.html', '<h2 id="act-1-prologue">Act 1, Prologue</h2>')
@@ -51,6 +52,39 @@ describe('Document Exporting', () => {
           .should('contain.text', '"content": "# Act I')
           .should('contain.text', '"content": "## Act 1, Prologue')
           .should('contain.text', '"content": "**Sampson**\\n\\nGregory, o\' my word, we\'ll not carry coals.')
+      })
+    })
+
+    describe('Current Card & Subtree', () => {
+      cy.get('#export-subtree').click()
+      describe('Word format', () => {
+        cy.get('#export-word').click()
+        cy.get('#export-preview', {log: false})
+          .should('not.contain.html', '<h1 id="act-i">Act I</h1>')
+          .should('not.contain.html', '<h2 id="act-1-prologue">Act 1, Prologue</h2>')
+          .should('not.contain.html', '<p><strong>Sampson</strong></p>\n<p>Gregory, o\' my word, we\'ll not carry coals.</p>')
+          .should('contain.html', '<h2 id="act-1-scene-3">Act 1, Scene 3</h2>')
+          .should('contain.html', '<p><strong>Lady Capulet</strong></p>\n<p>Enough of this; I pray thee, hold thy peace.</p>')
+      })
+
+      describe('Plain text', () => {
+        cy.get('#export-plain').click()
+        cy.get('#export-preview pre', {log: false})
+          .should('not.contain.text', '# Act I')
+          .should('not.contain.text', '## Act 1, Prologue')
+          .should('not.contain.text', '**Sampson**\n\nGregory, o\' my word, we\'ll not carry coals.')
+          .should('contain.text', '## Act 1, Scene 3')
+          .should('contain.text', '**Lady Capulet**\n\nEnough of this; I pray thee, hold thy peace.')
+      })
+
+      describe('JSON format', () => {
+        cy.get('#export-json').click()
+        cy.get('#export-preview pre', {log: false})
+          .should('not.contain.text', '"content": "# Act I')
+          .should('not.contain.text', '"content": "## Act 1, Prologue')
+          .should('not.contain.text', '"content": "**Sampson**\\n\\nGregory, o\' my word, we\'ll not carry coals.')
+          .should('contain.text', '"content": "## Act 1, Scene 3')
+          .should('contain.text', '"content": "**Lady Capulet**\\n\\nEnough of this; I pray thee, hold thy peace.')
       })
     })
   })
