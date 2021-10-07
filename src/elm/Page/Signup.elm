6bdd49c4d5bc1c2a9a0b2f7988a17e493194acb4
Adriano Ferrari <adriano.ferrari@gmail.com>
2021-10-07 14:31:59 -0400

    Update/tweak signup page design

--- src/elm/Page/Signup.elm
+++ src/elm/Page/Signup.elm
@@ -1,16 +1,18 @@
 module Page.Signup exposing (Model, Msg, init, subscriptions, toUser, update, view)
 
+import Ant.Icons.Svg as AntIcons
 import Browser.Dom
 import Html exposing (..)
-import Html.Attributes exposing (autocomplete, autofocus, class, classList, href, id, placeholder, src, type_, value)
-import Html.Events exposing (onBlur, onInput, onSubmit)
+import Html.Attributes exposing (autocomplete, autofocus, class, classList, for, href, id, src, type_, value)
+import Html.Events exposing (onClick, onInput, onSubmit)
 import Http exposing (Error(..))
 import Import.Template as Template
 import Route
 import Session exposing (Session)
+import Svg.Attributes
 import Task
 import Utils exposing (getFieldErrors)
-import Validate exposing (Valid, Validator, ifBlank, ifFalse, ifInvalidEmail, ifTrue, validate)
+import Validate exposing (Valid, Validator, ifBlank, ifInvalidEmail, ifTrue, validate)
 
 
 
@@ -21,7 +23,7 @@ type alias Model =
     { user : Session
     , email : String
     , password : String
-    , passwordConfirm : String
+    , showPassword : Bool
     , errors : List ( Field, String )
     }
 
@@ -30,12 +32,11 @@ type Field
     = Form
     | Email
     | Password
-    | PasswordConfirm
 
 
 init : Session -> ( Model, Cmd Msg )
 init user =
-    ( { user = user, email = "", password = "", passwordConfirm = "", errors = [] }
+    ( { user = user, email = "", password = "", showPassword = False, errors = [] }
     , Task.attempt (\_ -> NoOp) <| Browser.Dom.focus "signup-email"
     )
 
@@ -54,8 +55,7 @@ type Msg
     | SubmittedForm
     | EnteredEmail String
     | EnteredPassword String
-    | EnteredPassConfirm String
-    | Blurred Field
+    | ToggleShowPassword
     | CompletedSignup (Result Http.Error Session)
     | GotUser Session
 
@@ -76,37 +76,14 @@ update msg model =
                 Err errs ->
                     ( { model | errors = errs }, Cmd.none )
 
-        Blurred field ->
-            let
-                validator =
-                    case field of
-                        Email ->
-                            emailValidator
-
-                        Password ->
-                            passwordValidator
-
-                        PasswordConfirm ->
-                            passwordConfirmValidator
-
-                        Form ->
-                            ifTrue (always True) ( Form, "" )
-            in
-            case validate validator model of
-                Ok _ ->
-                    ( { model | errors = [] }, Cmd.none )
-
-                Err errs ->
-                    ( { model | errors = errs }, Cmd.none )
-
         EnteredEmail email ->
             ( { model | email = email }, Cmd.none )
 
         EnteredPassword password ->
             ( { model | password = password }, Cmd.none )
 
-        EnteredPassConfirm passwordConfirm ->
-            ( { model | passwordConfirm = passwordConfirm }, Cmd.none )
+        ToggleShowPassword ->
+            ( { model | showPassword = not model.showPassword }, Task.attempt (\_ -> NoOp) (Browser.Dom.focus "signup-password") )
 
         CompletedSignup (Ok user) ->
             ( { model | user = user }, Session.storeSignup user )
@@ -135,7 +112,7 @@ update msg model =
                         _ ->
                             fallbackMsg
             in
-            ( { model | errors = [ errorMsg ], password = "", passwordConfirm = "" }, Cmd.none )
+            ( { model | errors = [ errorMsg ], password = "" }, Cmd.none )
 
         GotUser user ->
             ( model, Route.replaceUrl (Session.navKey user) (Route.Import Template.WelcomeTree) )
@@ -157,20 +134,11 @@ passwordValidator =
         ]
 
 
-passwordConfirmValidator : Validator ( Field, String ) Model
-passwordConfirmValidator =
-    Validate.firstError
-        [ ifBlank .passwordConfirm ( PasswordConfirm, "Please enter your password twice." )
-        , ifFalse (\m -> m.password == m.passwordConfirm) ( PasswordConfirm, "Passwords do not match." )
-        ]
-
-
 modelValidator : Validator ( Field, String ) Model
 modelValidator =
     Validate.all
         [ emailValidator
         , passwordValidator
-        , passwordConfirmValidator
         ]
 
 
@@ -199,53 +167,51 @@ view model =
         passwordErrors =
             getFieldErrors Password model.errors
 
-        passwordConfirmErrors =
-            getFieldErrors PasswordConfirm model.errors
+        showHidePassword =
+            if model.showPassword then
+                div [ id "show-hide-password", onClick ToggleShowPassword ] [ AntIcons.eyeInvisibleOutlined [ Svg.Attributes.class "icon" ], text "Hide" ]
+
+            else
+                div [ id "show-hide-password", onClick ToggleShowPassword ] [ AntIcons.eyeOutlined [ Svg.Attributes.class "icon" ], text "Show" ]
     in
     div [ id "form-page" ]
-        [ a [ class "brand", href "{%HOMEPAGE_URL%}" ]
-            [ img [ id "logo", src "gingko-leaf-logo.svg" ] []
-            , h1 [] [ text "Gingko Writer" ]
-            ]
+        [ div [ class "page-backdrop" ] []
         , div [ class "page-bg" ] []
-        , h1 [ class "headline" ] [ text "Write better, faster." ]
+        , a [ class "brand", href "{%HOMEPAGE_URL%}" ] [ img [ id "logo", src "gingko-leaf-logo.svg" ] [] ]
+        , div [ class "form-header-container" ]
+            [ h1 [ class "headline" ] [ text "Welcome to Gingko Writer" ]
+            , p [ class "subtitle" ] [ text "Write the way you think, organize as you go, and let your words flow." ]
+            ]
         , div [ class "header" ] [ span [ class "alt-action" ] [ text "Already have an account? ", a [ href "/login" ] [ text "Login" ] ] ]
         , div [ class "center-form" ]
             [ form [ onSubmit SubmittedForm ]
                 [ div [] [ text (String.join "\n" formErrors) ]
                 , div [ class "input-error" ] [ text (String.join "\n" emailErrors) ]
+                , label [ for "singup-email" ] [ text "Email" ]
                 , input
                     [ id "signup-email"
-                    , placeholder "Email"
                     , classList [ ( "has-error", List.length emailErrors > 0 ) ]
                     , onInput EnteredEmail
                     , type_ "email"
                     , value model.email
                     , autofocus True
                     , autocomplete True
-                    , onBlur (Blurred Email)
                     ]
                     []
                 , div [ class "input-error" ] [ text (String.join "\n" passwordErrors) ]
+                , label [ for "singup-password" ] [ text "Password (7+ characters)", showHidePassword ]
                 , input
                     [ id "signup-password"
-                    , placeholder "Password (min. 7 characters)"
                     , onInput EnteredPassword
-                    , type_ "password"
+                    , type_
+                        (if model.showPassword then
+                            "text"
+
+                         else
+                            "password"
+                        )
                     , value model.password
                     , autocomplete True
-                    , onBlur (Blurred Password)
-                    ]
-                    []
-                , div [ class "input-error" ] [ text (String.join "\n" passwordConfirmErrors) ]
-                , input
-                    [ id "signup-password-confirm"
-                    , placeholder "Confirm Password"
-                    , onInput EnteredPassConfirm
-                    , type_ "password"
-                    , value model.passwordConfirm
-                    , autocomplete True
-                    , onBlur (Blurred PasswordConfirm)
                     ]
                     []
                 , button [ class "cta" ] [ text "Start Writing" ]
