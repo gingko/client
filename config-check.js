108e0ac36ca70f2226cfc35e27cb5afbc2fec9be
Adriano Ferrari <adriano.ferrari@gmail.com>
2022-02-04 10:13:14 -0500

    Use vanilla JS for config key xor check

--- config-check.js
+++ config-check.js
@@ -1,12 +1,13 @@
 const configReference = require("./config-example.js");
 const config = require("./config.js");
-const _ = require("lodash");
 const process = require("process");
 
 let confRefKeys = Object.keys(configReference).sort();
 let confKeys = Object.keys(config).sort();
 
-let diff = _.xor(confRefKeys, confKeys);
+let diff = confRefKeys
+            .filter(x => !confKeys.includes(x))
+            .concat(confKeys.filter(x => !confRefKeys.includes(x)));
 
 if (diff.length != 0) {
   process.stderr.write("Difference in config keys: " + JSON.stringify(diff));
