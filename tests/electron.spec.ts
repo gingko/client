c812f1092130b2c7f767098f1ccb038838c6d2df
Adriano Ferrari <adriano.ferrari@gmail.com>
2022-04-06 15:42:35 -0400

    Test file saving in Electron

--- tests/electron.spec.ts
+++ tests/electron.spec.ts
@@ -1,4 +1,7 @@
 import { _electron as electron, test, expect, ElectronApplication, Page, BrowserContext } from "@playwright/test";
+const path = require('path')
+import * as fs from 'fs/promises';
+import * as fsStd from 'fs';
 
 let electronApp: ElectronApplication;
 let homeWindow: Page;
@@ -28,6 +31,33 @@ test.describe('Check Home Page', async () => {
     newDocWindow = await electronApp.windows()[0]
     expect(newDocWindow).toBeTruthy();
     await expect(newDocWindow).toHaveTitle("Gingko Writer Desktop");
+  })
+
+  test('Can type in box and save', async  () => {
+    const textarea = await newDocWindow.locator('textarea.edit');
+    await textarea.focus()
+    await newDocWindow.keyboard.type('Hi!');
+    await expect(textarea).toHaveValue('Hi!')
+    await newDocWindow.keyboard.press('Control+Enter');
+
+    const savedCardView = await newDocWindow.locator('.card.active .view');
+    await expect(savedCardView).toHaveText('Hi!');
+  })
+
+  test('Saves to file properly', async () => {
+    const dir = '/home/adriano/Dropbox/Notes/';
+    const files = await fs.readdir(dir);
+    const latestFile =
+      files
+        .filter(async (file) => (await fs.stat(path.join(dir, file))).isFile())
+        .map((file) => {
+          let stats = fsStd.statSync(path.join(dir, file));
+          return {file: file, mtime: stats.mtime};
+        })
+        .sort((a, b) => b.mtime.getTime() - a.mtime.getTime())
+        .map(x => x.file)[0];
+    const fileContents = await fs.readFile(path.join(dir, latestFile), {encoding: "utf8"});
+    expect(fileContents).toEqual('<gingko-card id="1">\n\nHi!\n\n</gingko-card>');
   })
 })
 
