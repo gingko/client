d2720721420cc3ca758389a76479d91daa21879a
Adriano Ferrari <adriano.ferrari@gmail.com>
2022-04-13 08:54:32 -0400

    Rearrange e- main.js to create doc windows separately from home window

--- src/electron/main.js
+++ src/electron/main.js
@@ -5,8 +5,11 @@ import * as fs from 'fs/promises';
 
 let sha1Hash = crypto.createHash('sha1');
 
-const createWindow = () => {
-  const win = new BrowserWindow({
+let docWindows = {};
+
+
+const createHomeWindow = () => {
+  const homeWin = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -14,117 +17,86 @@ const createWindow = () => {
     }
   })
 
-  let isNew;
-  let openWindows = {};
-
-  ipcMain.on('clicked-new', async (event, title) => {
-    isNew = true;
-    const webContents = event.sender
-    const win = BrowserWindow.fromWebContents(webContents)
-
-    // Initialize New Document
-    let d = new Date();
-    let fileHash = sha1Hash.update(d.getTime()+"").digest('hex').slice(0,6);
-    let filePath = path.join(app.getPath('temp'), `Untitled-${d.toISOString().slice(0,10)}-${fileHash}.gkw`);
-    let filehandle = await fs.open(filePath, 'w');
-    openWindows[win.id] = {filePath : filePath, filehandle : filehandle, dirty: true};
-
-    // Prevent title being set from HTML
-    win.on('page-title-updated', (evt) => {
-      evt.preventDefault();
-    });
-
-    // Initialize Renderer
-    await win.loadFile(`${__dirname}/static/renderer.html`);
-    await webContents.send('file-received', {filePath : filePath, fileData : null})
-    win.setTitle(getTitleText(openWindows[win.id]));
-
-    // Prevent title being set from HTML
-    win.on('page-title-updated', (evt) => {
-      evt.preventDefault();
-    });
-  })
-
-  ipcMain.on('clicked-open', async (event, title) => {
-    const webContents = event.sender
-    const win = BrowserWindow.fromWebContents(webContents)
-
-    let dialogReturnValue = await dialog.showOpenDialog(win,
-  { properties: ['openFile']
-        , defaultPath: '/home/adriano/Dropbox/Notes/'
-        , filters:
-            [ {name:'Gingko Writer Document', extensions: ['gkw']}
-            , {name:'Gingko Desktop Legacy', extensions: ['gko']}
-            , {name:'Markdown Document', extensions: ['md']}
-            ]
-        });
-
-    if (dialogReturnValue.filePaths.length != 0) {
-      let filePath = dialogReturnValue.filePaths[0];
-      isNew = false;
-      let filehandle = await fs.open(filePath, 'r+');
-      openWindows[win.id] = {filePath: filePath, filehandle: filehandle, dirty : false};
-
-      // Load data
-      let fileData = await openWindows[win.id].filehandle.readFile({encoding: "utf8"});
-
-      // Initialize Renderer
-      await win.loadFile(`${__dirname}/static/renderer.html`);
-      await webContents.send('file-received', {filePath : filePath, fileData : fileData})
-      win.setTitle(getTitleText(openWindows[win.id]));
-
-      // Prevent title being set from HTML
-      win.on('page-title-updated', (evt) => {
-        evt.preventDefault();
-      });
-    }
-  })
-
-  ipcMain.on('set-dirty', (event, filePath, isDirty) =>{
-    const webContents = event.sender
-    const win = BrowserWindow.fromWebContents(webContents)
-    if (openWindows[win.id].dirty !== isDirty) {
-      console.log(isDirty);
-      openWindows[win.id].dirty = isDirty;
-      win.setTitle(getTitleText(openWindows[win.id]));
-    }
-  })
-
-  ipcMain.on('save-untitled', async (event, data) => {
-    const webContents = event.sender
-    const win = BrowserWindow.fromWebContents(webContents)
-
-    let {filePath, canceled} = await dialog.showSaveDialog(win, {defaultPath: app.getPath('documents')})
-    if (!canceled && filePath) {
-      openWindows[win.id].filePath = filePath;
-      openWindows[win.id].filehandle = await fs.open(filePath, 'w');
-      await openWindows[win.id].filehandle.write(data[1],0);
-      await webContents.send('file-saved', filePath);
-      win.setTitle(getTitleText(openWindows[win.id]));
-    }
-  })
-
-  ipcMain.on('save-file', async (event, data) =>{
-    console.time('save-file')
-    const webContents = event.sender
-    const win = BrowserWindow.fromWebContents(webContents)
-
-    await openWindows[win.id].filehandle.write(data[1], 0);
-    console.timeEnd('save-file')
-    openWindows[win.id].dirty = false;
-    win.setTitle(getTitleText(openWindows[win.id]));
-  })
-
-  win.loadFile(`${__dirname}/static/home.html`);
+  homeWin.loadFile(`${__dirname}/static/home.html`);
 }
 
+
+/* ==== IPC handlers ==== */
+
+ipcMain.on('clicked-new', async (event) => {
+  const webContents = event.sender
+  const homeWindow = BrowserWindow.fromWebContents(webContents)
+
+  homeWindow.hide();
+  await createNewDocWindow();
+  homeWindow.destroy();
+});
+
+ipcMain.on('clicked-open', async (event, title) => {
+  const webContents = event.sender
+  const homeWindow = BrowserWindow.fromWebContents(webContents)
+
+  let dialogReturnValue = await dialog.showOpenDialog(homeWindow,
+    { properties: ['openFile']
+      , defaultPath: app.getPath('documents')
+      , filters:
+        [ {name:'Gingko Writer Document', extensions: ['gkw']}
+          , {name:'Gingko Desktop Legacy', extensions: ['gko']}
+          , {name:'Markdown Document', extensions: ['md']}
+        ]
+    });
+
+  if (dialogReturnValue.filePaths.length != 0) {
+    homeWindow.hide();
+    await createDocWindow(dialogReturnValue.filePaths[0]);
+    homeWindow.destroy();
+  }
+})
+
+ipcMain.on('set-dirty', (event, filePath, isDirty) =>{
+  const webContents = event.sender
+  const win = BrowserWindow.fromWebContents(webContents)
+  if (docWindows[win.id].dirty !== isDirty) {
+    console.log(isDirty);
+    docWindows[win.id].dirty = isDirty;
+    win.setTitle(getTitleText(docWindows[win.id]));
+  }
+})
+
+ipcMain.on('save-untitled', async (event, data) => {
+  const webContents = event.sender
+  const win = BrowserWindow.fromWebContents(webContents)
+
+  let {filePath, canceled} = await dialog.showSaveDialog(win, {defaultPath: app.getPath('documents')})
+  if (!canceled && filePath) {
+    docWindows[win.id].filePath = filePath;
+    docWindows[win.id].filehandle = await fs.open(filePath, 'w');
+    await docWindows[win.id].filehandle.write(data[1],0);
+    await webContents.send('file-saved', filePath);
+    win.setTitle(getTitleText(docWindows[win.id]));
+  }
+})
+
+ipcMain.on('save-file', async (event, data) =>{
+  console.time('save-file')
+  const webContents = event.sender
+  const win = BrowserWindow.fromWebContents(webContents)
+
+  await docWindows[win.id].filehandle.write(data[1], 0);
+  console.timeEnd('save-file')
+  docWindows[win.id].dirty = false;
+  win.setTitle(getTitleText(docWindows[win.id]));
+})
+
+/* ==== Initialization ==== */
+
 app.whenReady().then(() => {
-  createWindow()
+  createHomeWindow()
 
   app.on('activate', () => {
     // On macOS re-create a window when the dock icon is clicked
     // and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createHomeWindow()
   })
 })
 
@@ -132,6 +104,68 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
+
+
+/* ==== Home and Doc Windows ==== */
+
+async function createNewDocWindow() {
+  const docWin = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // Initialize New Document
+  let d = new Date();
+  let fileHash = sha1Hash.update(d.getTime()+"").digest('hex').slice(0,6);
+  let filePath = path.join(app.getPath('temp'), `Untitled-${d.toISOString().slice(0,10)}-${fileHash}.gkw`);
+  let filehandle = await fs.open(filePath, 'w');
+  docWindows[docWin.id] = {filePath : filePath, filehandle : filehandle, dirty: true};
+
+  // Prevent title being set from HTML
+  docWin.on('page-title-updated', (evt) => {
+    evt.preventDefault();
+  });
+
+  // Initialize Renderer
+  await docWin.loadFile(`${__dirname}/static/renderer.html`);
+  await docWin.webContents.send('file-received', {filePath : filePath, fileData : null})
+  docWin.setTitle(getTitleText(docWindows[docWin.id]));
+
+  // Prevent title being set from HTML
+  docWin.on('page-title-updated', (evt) => {
+    evt.preventDefault();
+  });
+}
+
+
+async function createDocWindow(filePath) {
+  const docWin = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  let filehandle = await fs.open(filePath, 'r+');
+  docWindows[docWin.id] = {filePath: filePath, filehandle: filehandle, dirty : false};
+
+  // Load data
+  let fileData = await docWindows[docWin.id].filehandle.readFile({encoding: "utf8"});
+
+  // Initialize Renderer
+  await docWin.loadFile(`${__dirname}/static/renderer.html`);
+  await docWin.webContents.send('file-received', {filePath : filePath, fileData : fileData})
+  docWin.setTitle(getTitleText(docWindows[docWin.id]));
+
+  // Prevent title being set from HTML
+  docWin.on('page-title-updated', (evt) => {
+    evt.preventDefault();
+  });
+}
 
 
 /* ==== Helper Functions */
