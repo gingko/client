dd4e06e40bfe4e76f956d80ae5f7f057016cb559
Adriano Ferrari <adriano.ferrari@gmail.com>
2023-03-14 07:39:27 -0400

    Add secrets directory if it doesn't already exist

--- config_decrypt.sh
+++ config_decrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
+mkdir -p $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$CLIENT_CONFIG_PASSPHRASE" \
