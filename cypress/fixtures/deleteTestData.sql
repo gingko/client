12344adbb326b3167d83ecfc0cead9ea9e707e54
Adriano Ferrari <adriano.ferrari@gmail.com>
2023-05-09 20:06:39 -0400

    Also delete tree_snapshots of test user

--- cypress/fixtures/deleteTestData.sql
+++ cypress/fixtures/deleteTestData.sql
@@ -1,5 +1,6 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
+DELETE FROM tree_snapshots WHERE treeId IN (SELECT trees.id FROM trees WHERE owner='cypress@testing.com');
 DELETE FROM cards WHERE treeId IN (SELECT trees.id FROM trees WHERE owner='cypress@testing.com');
 DELETE FROM trees WHERE owner='cypress@testing.com';
 DELETE FROM users WHERE id='cypress@testing.com';
