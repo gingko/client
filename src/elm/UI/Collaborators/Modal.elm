6bcee636d075d05ff615a44bfff47cb9391c973b
Adriano Ferrari <adriano.ferrari@gmail.com>
2023-10-13 07:50:18 -0400

    add beta warning to UI.Collaboration.Modal

--- src/elm/UI/Collaborators/Modal.elm
+++ src/elm/UI/Collaborators/Modal.elm
@@ -2,7 +2,10 @@ module UI.Collaborators.Modal exposing (..)
 
 -- MODEL
 
-import Html exposing (Html, div, text)
+import Ant.Icons.Svg as AntIcon
+import Html exposing (Html, br, div, strong, text)
+import Html.Attributes exposing (class)
+import Svg.Attributes exposing (height, width)
 import Translation
 
 
@@ -21,5 +24,26 @@ init =
 
 view : Translation.Language -> Model -> List (Html msg)
 view lang model =
-    [ div [] [ text "Collaborators Modal" ]
+    [ div
+        [ class "bg-amber-100"
+        , class "border-amber-400"
+        , class "rounded-md"
+        , class "flex"
+        , class "gap-4"
+        , class "p-4"
+        , class "text-amber-900"
+        , class "fill-orange-600"
+        , class "items-center"
+        , class "mb-4"
+        ]
+        [ AntIcon.warningFilled [ width "30px", height "30px" ]
+        , div []
+            [ text "Realtime Collaboration is "
+            , strong [] [ text "currently in beta" ]
+            , text "."
+            , br [] []
+            , text "Please back up your document regularly while testing this feature."
+            ]
+        ]
+    , div [] [ text "Collaborators Modal" ]
     ]
