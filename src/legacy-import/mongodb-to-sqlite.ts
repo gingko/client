95cf614f296f7cc640b37a26201ae516323b4f13
Adriano Ferrari <adriano.ferrari@gmail.com>
2024-05-27 14:03:40 -0400

    save cards from mongodb-to-sqlite.ts into test SQLite db

--- src/legacy-import/mongodb-to-sqlite.ts
+++ src/legacy-import/mongodb-to-sqlite.ts
@@ -1,9 +1,26 @@
 import { Db, ObjectId, MongoClient, ServerApiVersion } from "mongodb";
 import * as hiddenConfig from '../../hidden-config.js';
 
+// SQLite setup
+import { Database } from 'bun:sqlite';
+const sqliteDb = new Database(hiddenConfig.IMPORT_TEST_SQLITE_DB_PATH);
+const createCardsTable = sqliteDb.query(`CREATE TABLE IF NOT EXISTS cards (id TEXT PRIMARY KEY, treeId TEXT, content TEXT, parentId TEXT, position FLOAT, updatedAt TEXT, deleted BOOLEAN)`);
+createCardsTable.run();
+const insertCard = sqliteDb.query(`INSERT INTO cards (id, treeId, content, parentId, position, updatedAt) VALUES ($id, $treeId, $content, $parentId, $position, $updatedAt)`);
+const insertCards = sqliteDb.transaction( cards => {
+  for (const card of cards) {
+    insertCard.run(card);
+  }
+});
+
+
+// MongoDB setup
 const client = new MongoClient(hiddenConfig.LEGACY_MONGODB_URI, {
   serverApi: ServerApiVersion.v1,
 });
+
+
+// Main function
 
 async function main() {
   try {
@@ -24,10 +41,9 @@ async function main() {
           return;
         }
 
-        const allTrees = await getUserTreesByEmail(db, Bun.argv[3]);
         const treeIds = Bun.argv[4].split(',');
-
-        const treesToConvert = allTrees.filter(tree => treeIds.includes(tree._id.toString()));
+        console.log('Getting trees:', treeIds);
+        const treesToConvert = await getUserTreesByIds(db, Bun.argv[3], treeIds);
 
         if (treesToConvert.length === 0) {
           console.error('No trees to convert');
@@ -35,6 +51,7 @@ async function main() {
         }
 
         for (const tree of treesToConvert) {
+          console.log('Converting tree:', tree._id);
           await convertTree(db, tree._id);
         }
       }
@@ -42,6 +59,19 @@ async function main() {
   } finally {
     await client.close();
   }
+}
+
+async function getUserTreesByIds(db : Db, email: string, treeIds: string[]) {
+  if (!db) {
+    throw new Error('Database not connected');
+  }
+
+  const user = await db.collection('users').findOne({ email });
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  return await db.collection('trees').find({ owner: user._id, _id: { $in: treeIds.map(id => new ObjectId(id)) } }).toArray();
 }
 
 async function getUserTreesByEmail(db : Db, email: string) {
@@ -63,10 +93,23 @@ async function convertTree(db: Db, treeId: ObjectId) {
     throw new Error('Database not connected');
   }
 
-  // Get cards for tree
-  const cards = await db.collection('cards').find({ treeId }).toArray();
-  console.log('Cards:', cards);
-  return cards;
+  // Get cards with treeId and deleted field null or not present
+  const cards = await db.collection('cards').find({ treeId, deleted: { $in: [null, false] } }).toArray();
+  const cardsSqlite = cards.map(cardMongoToSqlite);
+  console.log('Cards:', cardsSqlite.length);
+  insertCards(cardsSqlite);
+  return cardsSqlite;
+}
+
+function cardMongoToSqlite(card) {
+  return {
+    $id: card._id.toHexString(),
+    $treeId: card.treeId.toHexString(),
+    $content: card.content,
+    $parentId: card.parentId ? card.parentId.toHexString() : null,
+    $position: card.position,
+    $updatedAt: card.updatedAt.toISOString(),
+  };
 }
 
 main().catch(console.error);
