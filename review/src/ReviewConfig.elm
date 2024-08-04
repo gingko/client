397caec74df136037f3019be02627b83a871aa90
Adriano Ferrari <adriano.ferrari@gmail.com>
2024-08-04 09:13:27 -0400

    add elm-review

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -11,21 +11,49 @@ when inside the directory containing this file.
 
 -}
 
+import Docs.ReviewAtDocs
+import NoConfusingPrefixOperator
+import NoDebug.Log
+import NoDebug.TodoOrToString
+import NoExposingEverything
+import NoImportingEverything
+import NoMissingTypeAnnotation
+import NoMissingTypeAnnotationInLetIn
+import NoMissingTypeExpose
+import NoPrematureLetComputation
+import NoSimpleLetBody
 import NoUnused.CustomTypeConstructorArgs
 import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
 import NoUnused.Exports
 import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
-import Review.Rule exposing (Rule)
+import Review.Rule as Rule exposing (Rule)
+import Simplify
 
 
 config : List Rule
 config =
-    [ NoUnused.CustomTypeConstructors.rule []
-    , NoUnused.CustomTypeConstructorArgs.rule
-    , NoUnused.Exports.rule
-    , NoUnused.Parameters.rule
-    , NoUnused.Patterns.rule
-    , NoUnused.Variables.rule
+    [ --Docs.ReviewAtDocs.rule
+      --, NoConfusingPrefixOperator.rule
+      NoDebug.Log.rule
+    , NoDebug.TodoOrToString.rule
+        |> Rule.ignoreErrorsForDirectories [ "tests/" ]
+
+    --, NoExposingEverything.rule
+    --, NoImportingEverything.rule []
+    --, NoMissingTypeAnnotation.rule
+    --, NoMissingTypeAnnotationInLetIn.rule
+    --, NoMissingTypeExpose.rule
+    --, NoSimpleLetBody.rule
+    --, NoPrematureLetComputation.rule
+    --, NoUnused.CustomTypeConstructors.rule []
+    --, NoUnused.CustomTypeConstructorArgs.rule
+    --, NoUnused.Dependencies.rule
+    --, NoUnused.Exports.rule
+    --, NoUnused.Parameters.rule
+    --, NoUnused.Patterns.rule
+    --, NoUnused.Variables.rule
+    --, Simplify.rule Simplify.defaults
     ]
